2845ec298ed93fba9d5900d0955c353148c945b9
Yasith Ishara Perera <yasithishara@gmail.com>
2024-07-02 13:50:40 +0530

    second commit

--- src/main/java/lk/ijse/dep12/MyServlet4.java
+++ src/main/java/lk/ijse/dep12/MyServlet4.java
@@ -13,5 +13,10 @@ public class MyServlet4 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("<h1>Context Mapping</h1>");
+        resp.getWriter().println("<h1>URL:%s</h1>".formatted(req.getRequestURL()));
+        resp.getWriter().println("<h1>URI:%s</h1>".formatted(req.getRequestURI()));
+        resp.getWriter().println("<h1>ContextPath:%s</h1>".formatted(req.getContextPath()));
+        resp.getWriter().println("<h1>ServletPath:%s</h1>".formatted(req.getServletPath()));
+        resp.getWriter().println("<h1>PathInfo:%s</h1>".formatted(req.getPathInfo()));
     }
 }
